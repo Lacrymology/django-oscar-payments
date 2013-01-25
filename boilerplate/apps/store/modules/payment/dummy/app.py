410fc3ca20397a6abec153b5f9e00f4669488fe0
Tomas Neme <lacrymology@gmail.com>
2013-01-25 20:48:11 -0300

    missing module name

--- boilerplate/apps/store/modules/payment/dummy/app.py
+++ boilerplate/apps/store/modules/payment/dummy/app.py
@@ -8,9 +8,12 @@
 .. moduleauthor:: Tomas Neme <lacrymology@gmail.com>
 
 """
+from django.utils.translation import ugettext_lazy as _
 from boilerplate.apps.store.modules.payment.base.app import PaymentModule
 
 class DummyPaymentApplication(PaymentModule):
+    module_name = 'dummy'
+    verbose_name = _('Credit Cards')
     def get_urls(self):
         patterns = ('')
         return super(DummyPaymentApplication, self).get_urls()
