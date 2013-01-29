9d7a61de5d930d04ad952bbcfdeca315e69e0727
Tomas Neme <lacrymology@gmail.com>
2013-01-29 09:33:32 -0300

    start working on Dummy payment method views

--- boilerplate/apps/store/modules/payment/dummy/app.py
+++ boilerplate/apps/store/modules/payment/dummy/app.py
@@ -8,14 +8,20 @@
 .. moduleauthor:: Tomas Neme <lacrymology@gmail.com>
 
 """
+from django.conf.urls import patterns, url
 from django.utils.translation import ugettext_lazy as _
 from boilerplate.apps.store.modules.payment.base.app import PaymentModule
+from boilerplate.apps.store.modules.payment.dummy import views
 
 class DummyPaymentApplication(PaymentModule):
     module_name = 'dummy'
     verbose_name = _('Credit Cards')
     def get_urls(self):
-        patterns = ('')
-        return super(DummyPaymentApplication, self).get_urls()
+        base_patterns = super(DummyPaymentApplication, self).get_urls()
+        urlpatterns = patterns(
+            '',
+            url(r'^$', views.CollectBillingInfo.as_view(), name=self.get_root_url())
+        )
+        return urlpatterns + base_patterns
 
 application = DummyPaymentApplication()
---
+++ boilerplate/apps/store/modules/payment/dummy/views.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: boilerplate.apps.store.modules.payment.dummy.views
+   :platform: Unix
+   :synopsis: Views to collect and render
+
+.. moduleauthor:: Tomas Neme <lacrymology@gmail.com>
+
+"""
+from oscar.apps.checkout import views
+from oscar.apps.payment import forms
+
+class CollectBillingInfo(views.PaymentDetailsView):
+    """
+    Collect billing info and pass it on.
+    """
+    template_name = "checkout/bankcard_billing_form.html"
+    def get_context_data(self, **kwargs):
+        ctx = {
+            'bankcard_form': forms.BankcardForm(),
+            'billing_address_form': forms.BillingAddressForm(),
+        }
+        ctx.update(super(CollectBillingInfo, self).get_context_data(**kwargs))
+        return ctx
+
+    def post(self, request, *args, **kwargs):
+        if request.POST.get('action', '') == 'place_order':
+            return self.do_place_order(request)
+        bankcard_form = forms.BankcardForm(request.POST)
+        billing_address_form = forms.BillingAddressForm(request.POST)
+        if not all([bankcard_form.is_valid(), billing_address_form.is_valid()]):
+            self.preview = False
+            ctx = self.get_context_data(
+                bankcard_form=bankcard_form,
+                billing_address_form=billing_address_form)
+            return self.render_to_response(ctx)
+        self.preview = True
+        return self.render_preview(request,
+                                   bankcard_form=bankcard_form,
+                                   billing_address_form=billing_address_form)
