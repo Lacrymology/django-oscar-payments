34249397b7b0cfdbe9204afe4c318515bf670d8b
Tomas Neme <lacrymology@gmail.com>
2013-01-29 09:31:39 -0300

    maintain PaymentDetailsView and FormView functionality in checkout app

--- boilerplate/apps/store/apps/checkout/views.py
+++ boilerplate/apps/store/apps/checkout/views.py
@@ -9,20 +9,53 @@
 
 """
 from django import forms
-from django.views.generic import FormView
 from django.shortcuts import redirect
+from django.views.generic.edit import FormMixin
+from oscar.apps.checkout import views
 
 from boilerplate.apps.store import import_shop_app
 
-class PaymentDetailsView(FormView):
-    template_name = 'checkout/payment_details.html'
-    template_name_preview = 'checkout/preview.html'
-    preview = False
+class PaymentDetailsView(FormMixin, views.PaymentDetailsView):
+    """
+    We need to redefine post to acommodate both the FormView and
+    PaymentDetailView parts of this view.
 
+    In our case, this view should just render a form to choose the payment
+    method, but Oscar expects this view to handle order / payment details
+    preview as well
+    """
+
+    #: We change the normal template name because we want this to be a
+    #: "select payment" view
+
+    template_name = "checkout/select_payment.html"
     def __init__(self, *args, **kwargs):
         super(PaymentDetailsView, self).__init__(*args, **kwargs)
         self.app = import_shop_app('checkout', 'app').application
 
+    def get_context_data(self, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return super(PaymentDetailsView, self).get_context_data(form=form,
+                                                                **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        # oscar bit
+        error_response = self.get_error_response()
+        if error_response:
+            return error_response
+        if self.preview:
+            if request.POST.get('action', '') == 'place_order':
+                return self.submit(request.basket)
+            return self.render_preview(request, **kwargs)
+
+        # FormView bit
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
     def get_form_class(self):
         class PaymentModuleForm(forms.Form):
