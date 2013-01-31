293862624baaf770c70560046151dd66573ae36b
Tomas Neme <lacrymology@gmail.com>
2013-01-31 13:22:11 -0300

    BankCard + BillingAddress functionality should be pretty common, so I moved them to a mixin

--- boilerplate/apps/store/modules/payment/base/views.py
+++ boilerplate/apps/store/modules/payment/base/views.py
@@ -9,6 +9,8 @@
 
 """
 from oscar.apps.checkout import views
+from oscar.apps.payment import forms
+from oscar.apps.payment.exceptions import PaymentError
 
 class BaseRootMixin(object):
     """
@@ -47,6 +49,68 @@ class BaseRootMixin(object):
             }
         ctx.update(super(BaseRootMixin, self).get_context_data(**kwargs))
         return ctx
+
+class BankcardBillcardMixin(object):
+    """
+    Mixin object that sets the template names and adds the forms to the context
+    """
+    #: default bankcard/billing forms collect template
+    template_name = "checkout/bankcard_billing_form.html"
+    #: bankcard form class
+    bankcard_form_class = forms.BankcardForm
+    #: default bankcard/billing hidden forms preview template
+    template_name_preview = "checkuot/bankcard_billing_preview.html"
+    #: billing address form class
+    billing_address_form_class = forms.BillingAddressForm
+
+    def get_context_data(self, **kwargs):
+        """
+        Add the
+        """
+        ctx = {
+            'bankcard_form': self.get_form('bankcard'),
+            'billing_address_form': self.get_form('billing_address'),
+            }
+        ctx.update(super(BankcardBillcardMixin,
+                         self).get_context_data(**kwargs))
+        return ctx
+
+    def get_form(self, type=None, *args, **kwargs):
+        """
+        Return the requested form with the given arguments
+
+        :param type: REQUIRED: ('bankcard'|'billing_address')
+        :param args: positional arguments for the form constructor
+        :param kwargs: keyword arguments for the form constructor
+        :return: The form instance
+        """
+        if type not in ('bankcard', 'billing_address'):
+            raise PaymentError, "Form type parameter is required"
+        return getattr(self, '%s_form_class')(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handle posting of the collect and preview forms. This method doesn't
+        call super() so beware when overriding
+        """
+
+        # this is set by the preview template on the form. This should be more
+        # elegantly handled, I think
+        if request.POST.get('action', '') == 'place_order':
+            return self.do_place_order(request)
+        bankcard_form = self.get_form('bankcard', request.POST)
+        billing_address_form = self.get_form('billing_address', request.POST)
+        if not all([bankcard_form.is_valid(), billing_address_form.is_valid()]):
+            self.preview = False
+            ctx = self.get_context_data(
+                bankcard_form=bankcard_form,
+                billing_address_form=billing_address_form)
+            return self.render_to_response(ctx)
+        self.preview = True
+        return self.render_preview(request,
+                                   bankcard_form=bankcard_form,
+                                   billing_address_form=billing_address_form)
+
 
 class BaseRootView(BaseRootMixin, views.PaymentDetailsView):
     """
--- boilerplate/apps/store/modules/payment/dummy/views.py
+++ boilerplate/apps/store/modules/payment/dummy/views.py
@@ -11,34 +11,9 @@
 from boilerplate.apps.store.modules.payment.base import views
 from boilerplate.apps.store.modules.payment.dummy import forms
 
-class CollectBillingInfo(views.BaseRootView):
+class CollectBillingInfo(views.BankcardBillcardMixin, views.BaseRootView):
     """
     Collect billing info and pass it on.
     """
-    template_name = "checkout/bankcard_billing_form.html"
-    template_name_preview = "checkuot/bankcard_billing_preview.html"
-    def get_context_data(self, **kwargs):
-        ctx = {
-            'bankcard_form': forms.BankcardForm(),
-            'billing_address_form': forms.BillingAddressForm(),
-        }
-        ctx.update(super(CollectBillingInfo, self).get_context_data(**kwargs))
-        return ctx
-
-    def post(self, request, *args, **kwargs):
-        # this is set by the preview template on the form. This should be more
-        # elegantly handled, I think
-        if request.POST.get('action', '') == 'place_order':
-            return self.do_place_order(request)
-        bankcard_form = forms.BankcardForm(request.POST)
-        billing_address_form = forms.BillingAddressForm(request.POST)
-        if not all([bankcard_form.is_valid(), billing_address_form.is_valid()]):
-            self.preview = False
-            ctx = self.get_context_data(
-                bankcard_form=bankcard_form,
-                billing_address_form=billing_address_form)
-            return self.render_to_response(ctx)
-        self.preview = True
-        return self.render_preview(request,
-                                   bankcard_form=bankcard_form,
-                                   billing_address_form=billing_address_form)
+    bankcard_form_class = forms.BankcardForm
+    billing_address_form_class = forms.BillingAddressForm
